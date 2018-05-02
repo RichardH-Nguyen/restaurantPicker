f1ee20f98ce4558590b1ec4f44e9133d85be9216
su7163if <stuff@stuff.com>
2018-05-01 19:41:50 -0500

    made a page for displaying favorites with links to individual saved restaurant pages.

--- routes/saved.js
+++ routes/saved.js
@@ -5,7 +5,11 @@ var router = express.Router();
 var restaurants = require('../models/restaurants');
 
 router.get('/', function(req, res, next) {
-    res.render('/');
+    restaurants.find()
+        .then((docs) => {res.render('saved', {
+            title: 'saved restaurants',
+            restaurants:docs})
+    })
 });
 
 router.post('/add', function(req, res, next){
@@ -14,6 +18,18 @@ router.post('/add', function(req, res, next){
     r.save().then((newRestaurant) =>{console.log('created new restaurant.', newRestaurant)});
 
     res.render('index');
+});
+
+router.get('/:_id', function(req, res, next){
+    restaurants.findById(req.params._id)
+        .then((doc) => {
+        if(!doc){
+            res.status(404).send('Task not found')
+        }
+        else {
+            res.render('restaurant', {restaurant: doc})
+        }
+        })
 });
 
 module.exports = router;
